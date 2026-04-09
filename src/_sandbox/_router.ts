4c80611e2cc7ef11a3d07cc754835b1deab066ae
shizentaiga <tshizen2506@gmail.com>
2026-04-09 13:36:19 +0900

    HTMXテスト

--- src/_sandbox/_router.ts
+++ src/_sandbox/_router.ts
@@ -19,6 +19,7 @@ import { test02 } from './tests/02_db_check';
 import { test03 } from './tests/03_db_viewer';
 import { test04 } from './tests/04_script_test';
 import { test05 } from './tests/05_db_test';
+import { test06 } from './tests/06_htmx_check'; // 追記
 
 export const sandboxRouter = new Hono();
 
@@ -28,6 +29,7 @@ sandboxRouter.route('/test02', test02);
 sandboxRouter.route('/test03', test03);
 sandboxRouter.route('/test04', test04);
 sandboxRouter.route('/test05', test05);
+sandboxRouter.route('/test06', test06); // 追記
 
 // サンドボックスのトップページ（/_debug/）
 sandboxRouter.get('/', (c) => c.text("Sandbox Router is active."));
