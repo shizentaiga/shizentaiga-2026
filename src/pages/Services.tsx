56c4dccba56857533a612bad4d4345fe8930bef1
shizentaiga <tshizen2506@gmail.com>
2026-04-13 11:27:03 +0900

    コメントが表示される不具合修正

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -97,7 +97,6 @@ export const Services = async (c: Context) => {
 
       ${BookingFooter()}
 
-      /* --- 💡 CLIENT-SIDE INTERACTION --- */
       <script>
         /**
          * [Programmer Note] イベントリスナー
